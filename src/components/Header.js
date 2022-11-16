c3466b06b6bb5631b23d52a8bff06a0f1b7e8fed
Matthew-Mays <matthew.c.mays96@gmail.com>
2022-11-16 12:47:32 -0600

    remove App setup code and start on header component

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import redditLogo from "../images/reddit-logo.png";
+import "./header.css";
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <div></div>
+    </header>
+  );
+};
